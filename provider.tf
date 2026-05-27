88e8b7a101718835e1ee1f5b40e63a921a1d6bd0
LuanLimaZup <luan.gomes@zup.com.br>
2026-05-27 16:28:22 -0300

    fix: lint

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "6.44.0"
     }
   }
+  backend "s3" {
+    bucket = "aws-eks-terraform-945203151865-us-east-1-an"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
